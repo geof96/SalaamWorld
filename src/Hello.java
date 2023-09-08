b1e8f197cd19451c21cef372ae769c24ea7c282a
geof96 <leyton2730@gmail.com>
2023-09-08 13:21:20 +0200

    First commit

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+public static void main(String[]args){
+    System.out.println("Hello Gustav");
+
+    String greeting = "Hello Anas";
+    System.out.println(greeting);
+    int f = 27;
+    System.out.println(greeting);
+    System.out.println("How old are you?");
+    System.out.println(f);
+    String message = "Messi is the GOAT";
+
+    System.out.println(message);
+    String Message = "Messi is the Best";
+}
+}
